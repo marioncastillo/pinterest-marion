0345784e3563bae8cabd0f34f651dbb0b66eff80
“Marion” <marionacp@gmail.com>
2017-07-30 19:51:47 -0400

    estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,38 @@ for (var i = 0 ; i<40; i++){
 		console.log(datos[i].hashtag);
 		$(".fotos").append("<h6 class='hash'>" + datos[i].hashtag + "</h6><br>");
 	}
+
+for (var i = 0; i<40; i++){
+		console.log(datos[i].image_url);
+		$(".fotosdos").append("<div class='lista'> <div class='contener'><img src='dist/img/" + datos[i].image_url + "'></div></div>");
+		
+		console.log(datos[i].title);
+		$(".fotosdos").append("<h4 class='titulo'>" + datos[i].title + "</h4>" );
+		
+		console.log(datos[i].description);
+		$(".fotosdos").append("<h5 class='descripcion'>" + datos[i].description + "</h5>");
+
+		console.log(datos[i].user);
+		$(".fotosdos").append("<h6 class='usuario'>" + datos[i].user + "</h6>");
+		
+		console.log(datos[i].hashtag);
+		$(".fotosdos").append("<h6 class='hash'>" + datos[i].hashtag + "</h6><br>");
+	}
+
+for (var i = 20; i<40; i++){
+		console.log(datos[i].image_url);
+		$(".fotostres").append("<div class='lista'> <div class='contener'><img src='dist/img/" + datos[i].image_url + "'></div></div>");
+		
+		console.log(datos[i].title);
+		$(".fotostres").append("<h4 class='titulo'>" + datos[i].title + "</h4>" );
+		
+		console.log(datos[i].description);
+		$(".fotostres").append("<h5 class='descripcion'>" + datos[i].description + "</h5>");
+
+		console.log(datos[i].user);
+		$(".fotostres").append("<h6 class='usuario'>" + datos[i].user + "</h6>");
+		
+		console.log(datos[i].hashtag);
+		$(".fotostres").append("<h6 class='hash'>" + datos[i].hashtag + "</h6><br>");
+	}
+
--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,13 @@ var minifyCSS = require('gulp-minify-css');
 var webserver = require('gulp-webserver');
 
 gulp.task('script', function(){
-	gulp.src(['node_modules/jquery/dist/jquery.js', 'assets/js/*.js'])
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.js','assets/js/*.js'])
 	.pipe(concat('script.js'))
 	.pipe(gulp.dest('dist/js/'))
 });
 
 gulp.task('style', function(){
-	gulp.src(['assets/sass/main.scss'])
+	gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css','assets/sass/main.scss'])
 	.pipe(sass().on('error', sass.logError))
 	.pipe(minifyCSS())
 	.pipe(concat('style.min.css'))
